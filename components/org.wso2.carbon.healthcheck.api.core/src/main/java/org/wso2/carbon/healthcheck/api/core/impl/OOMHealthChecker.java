ebb71c16f83fbd1f084773377616748ca1353bb9
pedro@wso2.com <pedro@wso2.com>
2021-04-19 16:11:21 -0300

    Refactoring the method to process the checkHealth;Removing unused code; logging the error

--- components/org.wso2.carbon.healthcheck.api.core/src/main/java/org/wso2/carbon/healthcheck/api/core/impl/OOMHealthChecker.java
+++ components/org.wso2.carbon.healthcheck.api.core/src/main/java/org/wso2/carbon/healthcheck/api/core/impl/OOMHealthChecker.java
@@ -16,45 +16,20 @@
 
 package org.wso2.carbon.healthcheck.api.core.impl;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.tomcat.jdbc.pool.ConnectionPool;
-import org.apache.tomcat.jdbc.pool.DataSourceProxy;
 import org.wso2.carbon.healthcheck.api.core.AbstractHealthChecker;
 import org.wso2.carbon.healthcheck.api.core.Constants;
 import org.wso2.carbon.healthcheck.api.core.exception.BadHealthException;
 import org.wso2.carbon.healthcheck.api.core.exception.HealthCheckError;
-import org.wso2.carbon.healthcheck.api.core.exception.HealthCheckFailedException;
 import org.wso2.carbon.healthcheck.api.core.model.HealthCheckerConfig;
-import org.wso2.carbon.healthcheck.api.core.util.LambdaExceptionUtils;
-import org.wso2.carbon.healthcheck.api.core.util.Utils;
-import org.wso2.carbon.ndatasource.common.DataSourceException;
-import org.wso2.carbon.ndatasource.core.CarbonDataSource;
-import org.wso2.carbon.ndatasource.core.DataSourceManager;
-import org.wso2.carbon.ndatasource.core.JNDIConfig;
-
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
-import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Properties;
-import java.util.Set;
-import javax.management.AttributeNotFoundException;
-import javax.management.InstanceNotFoundException;
-import javax.management.MBeanException;
-import javax.management.MBeanServer;
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectInstance;
-import javax.management.ObjectName;
-import javax.management.ReflectionException;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 /**
  * This HealthChecker will check the health of the Heap Memory to validate if 
@@ -78,27 +53,24 @@ public class OOMHealthChecker extends AbstractHealthChecker {
 
     @Override
     public Properties checkHealth() throws BadHealthException {
-        List<HealthCheckError> errors = new ArrayList<>();
-        Properties cumulativeResults = new Properties();
-        isOOM(cumulativeResults);
-        return cumulativeResults;
+        return isOOM();
     }
     
-    private void isOOM(Properties cumulativeResults) throws BadHealthException {
+    private Properties isOOM() throws BadHealthException {
 		MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
 		MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
 		
 		if (heapMemoryUsage.getMax() == heapMemoryUsage.getCommitted()) {
-			cumulativeResults.setProperty(OOM_PROP, "true");
 			List<HealthCheckError> errors = new ArrayList<>();
-			errors.add(new HealthCheckError(ERROR_OOM,
+			errors.add(new HealthCheckError(Constants.ErrorCodes.ERROR_OOM,
                     "The server is out of memory", "java.lang.OutOfMemoryError: Java heap space"));
-			throw new BadHealthException("Out Of Memory Health Check", errors);
-		} else {
-			cumulativeResults.setProperty(OOM_PROP, "false");
+			BadHealthException badHealthException = new BadHealthException("Out Of Memory Health Check", errors);
+			log.error(badHealthException);
+			throw badHealthException;
 		}
 		
+		Properties cumulativeResults = new Properties();
+		cumulativeResults.setProperty(OOM_PROP, "false");
+		return cumulativeResults;
 	}
-
-
 }
